7cb7f80af3ce3c9db9b97efba1ba4bd804a0effe
Ryan Zigler <rpzigler@gmail.com>
2020-08-06 19:37:35 -0500

    setup AirBNB style guide for eslint, changed from Faker to Fakerator for generating fake data, and impletmented rules table seed in fakerator.js

--- server/database/fakerator/fakerator.js
+++ server/database/fakerator/fakerator.js
@@ -1,30 +1,57 @@
-var Fakerator = require('fakerator');
-var fakerator = Fakerator();
+const Fakerator = require('fakerator');
 
-fakerator.random.boolean();
+const fakerator = Fakerator();
 
-
-/*========== RULES TABLE==========*/
+/* ========== RULES TABLE========== */
 
 // needed for check_in_start, check_in_end, and check_out
 // mySQL TIME format: HH:MM:SS or 'hhmmss'. 103000 = 10:30am; 230000 = 11:00pm
-fakerator.random.number(max);
-fakerator.random.number(min, max);
 
 const checkInStart = () => {
-  var minArray = [0, 15, 30, 45];
-  var hour = fakerator.random.number(5, 13);
-  var minutes = fakerator.arrayElement(minArray);
+  const minArray = [0, 15, 30, 45];
+  let hour = fakerator.random.number(5, 13);
+  let minutes = fakerator.arrayElement(minArray);
 
-  if (hour < 10) {
-    hour = 0 + hour.toString();
-  } else if (hour >= 10) {
-    hour = hour.toString();
-  }
+  hour = hour < 10 ? 0 + hour.toString() : hour.toString();
+  minutes = minutes === 0 ? 0 + minutes.toString() : minutes.toString();
 
-  if (minutes === 0) {
-    minutes = 0 + minutes.toString();
-  } else if (minutes !== 0) {
-    minutes = minutes.toString();
-  }
+  return `${hour}${minutes}00`;
 };
+
+const checkInEnd = () => {
+  const minArray = [0, 15, 30, 45];
+  let hour = fakerator.random.number(14, 23);
+  let minutes = fakerator.arrayElement(minArray);
+
+  hour = hour < 10 ? 0 + hour.toString() : hour.toString();
+  minutes = minutes === 0 ? 0 + minutes.toString() : minutes.toString();
+
+  return `${hour}${minutes}00`;
+};
+
+const checkOut = () => {
+  const minArray = [0, 15, 30, 45];
+  let hour = fakerator.random.number(23);
+  let minutes = fakerator.arrayElement(minArray);
+
+  hour = hour < 10 ? 0 + hour.toString() : hour.toString();
+  minutes = minutes === 0 ? 0 + minutes.toString() : minutes.toString();
+
+  return `${hour}${minutes}00`;
+};
+
+const kidFriendly = fakerator.random.boolean() === true ? 1 : 0;
+
+const creditCards = fakerator.random.boolean() === true ? 1 : 0;
+
+const ageRestriction = fakerator.random.boolean() === true ? 1 : 0;
+
+const curfew = fakerator.random.boolean() === true ? 1 : 0;
+
+const lockOut = fakerator.random.boolean() === true ? 1 : 0;
+
+const nonSmoking = fakerator.random.boolean() === true ? 1 : 0;
+
+const petFriendly = fakerator.random.boolean() === true ? 1 : 0;
+
+const taxesIncluded = fakerator.random.boolean() === true ? 1 : 0;
