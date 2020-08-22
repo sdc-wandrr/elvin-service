931fcfc04390baabde0c3c1010540a2cccaf86bd
Ryan Zigler <rpzigler@gmail.com>
2020-08-22 17:53:09 -0500

    added favicon

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const query = require('./database/queries.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/hostels/:id', express.static(path.join(__dirname, '../public')));
 
