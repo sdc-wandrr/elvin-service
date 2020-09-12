51b231f4ec5fcc115c68bc135c91efd1d12ae0d8
Elvin Kosova <elvinkosova@gmail.com>
2020-09-11 23:46:36 -0700

    Prepare queries

--- server/database/postgresql/queries.js
+++ server/database/postgresql/queries.js
@@ -1,36 +1,61 @@
 const db = require('./client.js');
 
 const getRecordById = (id) => {
-  const q = 'SELECT * FROM hostels WHERE id = $1';
-  return db.query(q, [id]);
+  const query = 'SELECT * FROM hostels WHERE id = $1';
+  const q = {
+    name: 'record-by-id',
+    text: query,
+    values: [id],
+  };
+  return db.query(q);
 };
 
 const getHostelById = (id) => {
-  const q = 'SELECT hostel_name FROM hostels WHERE id = $1';
-  return db.query(q, [id]);
+  const query = 'SELECT hostel_name FROM hostels WHERE id = $1';
+  const q = {
+    name: 'hostel-by-id',
+    text: query,
+    values: [id],
+  };
+  return db.query(q);
 };
 
 const getAddressByHostelId = (id) => {
-  const q = `SELECT street_address, city, state, zip, country,
+  const query = `SELECT street_address, city, state, zip, country,
   country_code, latitude, longitude FROM hostels WHERE id = $1`;
-  return db.query(q, [id]);
+  const q = {
+    name: 'address-by-hostel-id',
+    text: query,
+    values: [id],
+  };
+  return db.query(q);
 };
 
 const getRulesByHostelId = (id) => {
-  const q = `SELECT check_in_start, check_in_end, check_out,
+  const query = `SELECT check_in_start, check_in_end, check_out,
   kid_friendly, credit_cards, age_restriction, curfew,
   lock_out, non_smoking, pet_friendly, taxes_included,
   cancellation, important_notes_one, important_notes_two,
   important_notes_three, important_notes_four, important_notes_five
   FROM hostels WHERE id = $1`;
-  return db.query(q, [id]);
+  const q = {
+    name: 'rules-by-hostel-id',
+    text: query,
+    values: [id],
+  };
+  return db.query(q);
 };
 
 const getDescriptionByHostelId = (id) => {
-  const q = `SELECT editorial_text_one, editorial_text_two,
+  const query = `SELECT editorial_text_one, editorial_text_two,
   description_text_one, description_text_two, description_text_three
   FROM hostels WHERE id = $1`;
-  return db.query(q, [id]);
+  const q = {
+    name: 'description-by-hostel-id',
+    text: query,
+    values: [id],
+  };
+  return db.query(q);
 };
 
 module.exports.getRecordById = getRecordById;
