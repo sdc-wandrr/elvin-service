402df850d8db074e475b61731ff5e8d138a79000
Elvin Kosova <elvinkosova@gmail.com>
2020-09-27 15:41:22 -0700

    Optimize bundle scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './client/index.jsx',
   output: {
     path: path.resolve(__dirname, 'public'),
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,5 @@ const { merge } = require('webpack-merge');
 const dev = require('./webpack.config.js');
 
 module.exports = merge(dev, {
-  mode: 'development',
+  mode: 'production',
 });
